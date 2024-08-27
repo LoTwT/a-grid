042aa6934b3998bc257aebc59a1c74a7c9933b87
Lo <l709937065@gmail.com>
2024-08-27 11:55:11 +0800

    chore(playground): init

---
+++ playground/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { AGrid, AGridItem } from "a-grid"
+</script>
+
+<template>
+  <a-grid tag="section">
+    <a-grid-item start="2" span="2" style="background-color: red">
+      1
+    </a-grid-item>
+    <a-grid-item tag="span" :start="6" span="2" style="background-color: pink">
+      2
+    </a-grid-item>
+    <a-grid-item tag="a" :span="3" style="background-color: lightblue">
+      3
+    </a-grid-item>
+    <a-grid-item tag="aside" start="12" style="background-color: lightcoral">
+      4
+    </a-grid-item>
+  </a-grid>
+</template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+</style>
